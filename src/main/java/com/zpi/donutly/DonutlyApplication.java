f155dcc9c64347843b6d225fe1e68a8a394fbdb3
Piotr <Jonczyk.piotrr@gmail.com>
2022-03-24 21:28:05 +0100

    Initial commit

---
+++ src/main/java/com/zpi/donutly/DonutlyApplication.java
@@ -0,0 +1,13 @@
+package com.zpi.donutly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DonutlyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zpi/donutly/DonutlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zpi.donutly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
