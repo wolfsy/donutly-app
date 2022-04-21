70d339763e705c18e9473730d034f1c7ff04d7db
Piotr <Jonczyk.piotrr@gmail.com>
2022-04-22 00:42:47 +0200

    Swagger proper functionality addition to project

---
+++ src/main/java/com/zpi/donutly/config/SpringFoxConfig.java
@@ -0,0 +1,27 @@
+package com.zpi.donutly.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+    // http://localhost:8080/swagger-ui.html - ui
+
+    //TODO: spring security konfiguracja dostępu do ścieżek
+
+    @Bean
+    public Docket apiResponse() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
