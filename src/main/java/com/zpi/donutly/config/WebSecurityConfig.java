6dbd61e050c6b223a2e20401e27ad55a3cd504e8
Piotr <Jonczyk.piotrr@gmail.com>
2022-04-26 20:19:27 +0200

    Cors Filter addition to WebSecurityConfig

--- src/main/java/com/zpi/donutly/config/WebSecurityConfig.java
+++ src/main/java/com/zpi/donutly/config/WebSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.zpi.donutly.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +18,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests()
                 .anyRequest().permitAll().and().csrf().disable().cors();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedOrigin("http://localhost:3000");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsFilter(source);
     }
 }
