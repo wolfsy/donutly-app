83bfac5bb2cc888c4a94d383df831a1c23929676
UsefulSpider <pawelwnuk98@gmail.com>
2022-05-22 14:07:43 +0200

    Implemented endpoint to get all donations of given user id

--- src/main/java/com/zpi/donutly/repository/DepositRepository.java
+++ src/main/java/com/zpi/donutly/repository/DepositRepository.java
@@ -4,10 +4,14 @@ import com.zpi.donutly.model.Deposit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepositRepository extends JpaRepository<Deposit, Long> {
 
     Deposit findDepositById(Long id);
 
     Deposit findFirstIdByOrderByIdDesc();
+
+    List<Deposit> findAllByUserId(Long userId);
 }
