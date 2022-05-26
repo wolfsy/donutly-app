5267767cf29c9fc77d27788828361ce96366e9c5
Grzegorz_Wisniewski <grzechuwisniewski18@gmail.com>
2022-05-26 16:05:13 +0200

    Implement handle of donation visibility change btn

--- src/main/webapp/reactjs/src/components/user/details/UserDonationListItem.jsx
+++ src/main/webapp/reactjs/src/components/user/details/UserDonationListItem.jsx
@@ -1,20 +1,41 @@
-import { useState, useEffect } from 'react';
-import { Row, Col, Card } from 'react-bootstrap';
+import { useState, useEffect, useRef } from 'react';
+import { Row, Col, Card, Spinner } from 'react-bootstrap';
+
+import DepositService from '../../../services/DepositService';
 
 function UserDonationListItem({ donation, token }) {
 
     const [date, setDate] = useState(null);
+    const [visibility, setVisibility] = useState(donation.visibility);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMsg, setErrorMsg] = useState('');
+    const errorRef = useRef();
 
     useEffect(() => {
         var time = new Date(donation.time);
         const strTime = time.toLocaleString();
         setDate(strTime.substring(0, strTime.length - 3));
-    }, [donation.time])
+    }, [donation.time]);
+
+    const changeVisibility = async () => {
+        try {
+            setErrorMsg('');
+            setIsLoading(true);
+            const response = await DepositService.changeDepositVisibilityById(donation.id, !visibility);
+            setIsLoading(false);
+            setVisibility(response?.data.visibility);
+        }
+        catch(err) {
+            setIsLoading(false);
+            setErrorMsg('Error while changing visibility');
+            errorRef.current.focus();
+        }
+    }
 
   return (
         <Row className="d-flex justify-content-center">
             <Col xs={11} xl={10}>
-                <Card className="my-3 user-card p-3 shadow-sm">
+                <Card className={`my-3 user-card p-3 ${!visibility ? 'hidden-card-border' : ''}`}>
                     <Card.Body>
                         <Row>
                             <Col xs={12} sm={12} md={12} lg={4} xl={4} xxl={3}
@@ -32,11 +53,28 @@ function UserDonationListItem({ donation, token }) {
                                 <p className="mb-5 fst-italic">{donation.amount} PLN</p>
                                 {
                                     token?.decoded?.role === 'ADMIN' &&
-                                    <button 
+                                    <div>
+                                        <button 
                                         className="app-button donation-card-btn mt-auto"
-                                    >
-                                        Hide
-                                    </button>
+                                        onClick={changeVisibility}
+                                        disabled={isLoading}
+                                        >
+                                            {
+                                                isLoading ? 
+                                                <Spinner
+                                                as="span"
+                                                animation="border"
+                                                size="sm"
+                                                role="status"
+                                                aria-hidden="true"
+                                                /> : 
+                                                <span>{visibility ? "Hide" : "Show"}</span>
+                                            }
+                                        </button>
+                                        <p className="text-danger mt-3" ref={errorRef} aria-live="assertive">
+                                            {errorMsg}
+                                        </p>
+                                    </div>
                                 }
                             </Col>
                         </Row>
--- src/main/webapp/reactjs/src/services/DepositService.js
+++ src/main/webapp/reactjs/src/services/DepositService.js
@@ -5,7 +5,16 @@ const DEPOSIT_API_BASE_URL = 'http://localhost:8080/api/deposit/';
 class DepositService {
 
     async getAllDepositsByUserId(userId) {
-        return await axios.get(DEPOSIT_API_BASE_URL + `all/${userId}`);
+        const response = await axios.get(DEPOSIT_API_BASE_URL + `all/${userId}`);
+
+        return response;
+    }
+
+    async changeDepositVisibilityById(depositId, visibility) {
+        const response = await axios.patch(DEPOSIT_API_BASE_URL +
+             `visibility/${depositId}/${visibility}`);
+            
+        return response;
     }
 
 }
